b8064a1cd2240695bcae328347ae90275a68bcec
Giana Blantin <giana@blantin.com>
2024-02-02 16:58:26 -0800

    📦 Remove LightningCSS

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,3 @@
-import browserslist from 'browserslist';
-import { browserslistToTargets } from 'lightningcss';
-
 export default {
-  base: '/snap-scroll-animation/',
-  css: {
-    transformer: 'lightningcss',
-    lightningcss: {
-      targets: browserslistToTargets(
-        browserslist('last 2 versions, >= 0.25%, not dead')
-      ),
-    },
-  },
-  build: {
-    cssMinify: 'lightningcss',
-  },
+  base: '/snap-scroll-animation/'
 };
